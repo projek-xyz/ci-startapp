52e7ac8265c53e1e8ce7ab18bd07e2b299dc5269
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-11-18 03:43:55 +0700

    Update configuration files
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/config/hooks.php
+++ app/config/hooks.php
@@ -1,4 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once PROJEK_COMMON_PATH . 'mod/config/hooks.php';
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend CI without hacking the core
+| files.  Please see the user guide for info:
+|
+|   http://codeigniter.com/user_guide/general/hooks.html
+|
+*/
+
+$path = str_replace(FCPATH, '', PROJEK_COMMON_PATH);
+$_base = [
+    'filename' => 'bootstrap.php',
+    'filepath' => '../vendor/projek-xyz/ci-common/mod'
+];
+
+$hook['pre_system'] = array_merge($_base, [
+    'function' => 'pre_system_hook'
+]);
+
+if (defined('PHPUNIT_TEST')) {
+    $hook['display_override'] = array_merge($_base, [
+        'function' => 'display_override_hook'
+    ]);
+}
+
